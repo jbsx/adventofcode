e6ca5a0ad6964f74ffd01bf1ee9e8046d3f09f4a
= <=>
2022-12-16 10:10:18 +0530

    bruh

--- 2022/day15.py
+++ 2022/day15.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 lines = [i.strip().split(": ") for i in open("inputday15", "r").readlines()]
 
+answerrow = 10
 sensor = []
 beacon = []
 for i in lines:
@@ -15,3 +16,23 @@ for i in lines:
     x = int(j.split(", ")[0].split("=")[1])
     y = int(j.split(", ")[1].split("=")[1])
     beacon.append([x, y])
+
+length = 0
+for i in range(len(sensor)):
+    sx, sy, bx, by = sensor[i][0], sensor[i][1], beacon[i][0], beacon[i][1]
+    dist = (sx - bx) + (sy - by)
+    length = max(length, sx + (dist - abs(sy - answerrow)))
+
+row = [0] * length
+for i in range(len(sensor)):
+    sx, sy, bx, by = sensor[i][0], sensor[i][1], beacon[i][0], beacon[i][1]
+    dist = (sx - bx) + (sy - by)
+    cleared = dist - abs(sy - answerrow)
+    print(cleared)
+    f, t = sx - cleared // 2, sx + cleared // 2
+
+res = 0
+for i in row:
+    if i == 1:
+        res += 1
+print("Answer to part 1: ", row)
